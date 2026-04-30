da7202c9ff81159cdaf9c8c6cee3476f85a18db5
TheMigo9 <162376189+TheMigo9@users.noreply.github.com>
2026-04-30 20:10:53 +0300

    Add files via upload

---
+++ assets/preload/data/technophobia/script.lua
@@ -0,0 +1,7 @@
+function onCreate()	
+    if "health" < 1 then
+	setProperty('healthBarBG.visible', false)
+	setProperty('healthBar.visible', false)
+	setProperty('iconP1.visible', false)
+	setProperty('iconP2.visible', false)
+end end  
